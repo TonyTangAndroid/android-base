b8a98f925b27e4afa54279a48794ca18fff10e26
ztang <ztang@microstrategy.com>
2017-01-09 14:52:27 -0500

    add parse error

---
+++ gsoncustomizedserializer/src/main/java/com/gson/test/ErrorBean.java
@@ -0,0 +1,32 @@
+package com.gson.test;
+
+public class ErrorBean {
+    private String message;
+    private int code;
+
+    public ErrorBean(String message, int code) {
+        this.message = message;
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
