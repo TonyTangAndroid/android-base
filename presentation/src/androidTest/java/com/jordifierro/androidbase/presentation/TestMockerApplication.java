62f91ef5671f46c4be3f1059e9940983f4854379
TonyTandAndroid <tonytangandroid@gmail.com>
2017-04-12 23:45:08 -0400

    demo compile error

--- presentation/src/androidTest/java/com/jordifierro/androidbase/presentation/TestMockerApplication.java
+++ presentation/src/androidTest/java/com/jordifierro/androidbase/presentation/TestMockerApplication.java
@@ -6,8 +6,6 @@ public class TestMockerApplication extends BaseApplication {
 
     @Override
     public FragmentInjector getFragmentInjector() {
-        return DaggerTestMockerComponent.builder()
-                .applicationComponent(this.applicationComponent)
-                .testMockerModule(new TestMockerModule()).build();
+        throw new RuntimeException("");
     }
 }
