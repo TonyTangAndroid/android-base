6091f2fd1678ca9d522ffb08221992bdb95a8d76
TonyTangAndroid <tonythompsoncmu@gmail.com>
2019-02-16 21:59:12 -0500

    Remove explicit provider of ViewModelFactory

--- app/src/main/java/com/tony/tang/note/ui/feature/note/list/ViewModelModule.java
+++ app/src/main/java/com/tony/tang/note/ui/feature/note/list/ViewModelModule.java
@@ -1,12 +1,9 @@
 package com.tony.tang.note.ui.feature.note.list;
 
-import com.tony.tang.note.app.AppScope;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.util.Map;
 
 import javax.inject.Provider;
 
@@ -22,14 +19,8 @@ public class ViewModelModule {
     @Provides
     @IntoMap
     @ViewModelKey(NoteBeanAndroidViewModel.class)
-    ViewModel noteBeanAndroidViewModel(Provider<NoteBeanAndroidViewModel> liveDataProvider) {
-        return liveDataProvider.get();
-    }
-
-    @AppScope
-    @Provides
-    ViewModelFactory viewModelFactory(Map<Class<? extends ViewModel>, Provider<ViewModel>> providerMap) {
-        return new ViewModelFactory(providerMap);
+    ViewModel noteBeanAndroidViewModel(Provider<NoteBeanAndroidViewModel> provider) {
+        return provider.get();
     }
 
     @Target(ElementType.METHOD)
