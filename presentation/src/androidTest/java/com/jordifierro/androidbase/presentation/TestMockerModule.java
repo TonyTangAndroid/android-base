0c8be075338c66f19c6013267bd09d2717353fbf
TonyTandAndroid <tonytangandroid@gmail.com>
2020-01-28 02:33:06 -0800

    [Revertible] Remove android unit test

--- presentation/src/androidTest/java/com/jordifierro/androidbase/presentation/TestMockerModule.java
+++ presentation/src/androidTest/java/com/jordifierro/androidbase/presentation/TestMockerModule.java
@@ -2,13 +2,6 @@ package com.jordifierro.androidbase.presentation;
 
 import com.jordifierro.androidbase.presentation.dependency.ActivityScope;
 import com.jordifierro.androidbase.presentation.presenter.LoginPresenter;
-import com.jordifierro.androidbase.presentation.presenter.NoteCreatePresenter;
-import com.jordifierro.androidbase.presentation.presenter.NoteDetailPresenter;
-import com.jordifierro.androidbase.presentation.presenter.NoteEditPresenter;
-import com.jordifierro.androidbase.presentation.presenter.NotesPresenter;
-import com.jordifierro.androidbase.presentation.presenter.RegisterPresenter;
-import com.jordifierro.androidbase.presentation.presenter.ResetPasswordPresenter;
-import com.jordifierro.androidbase.presentation.presenter.SettingsPresenter;
 
 import org.mockito.Mockito;
 
@@ -22,48 +15,6 @@ public class TestMockerModule {
     @ActivityScope
     LoginPresenter provideLoginPresenter() {
         return Mockito.mock(LoginPresenter.class);
-    }
-
-    @Provides
-    @ActivityScope
-    NoteCreatePresenter provideNoteCreatePresenter() {
-        return Mockito.mock(NoteCreatePresenter.class);
-    }
-
-    @Provides
-    @ActivityScope
-    NoteDetailPresenter provideNoteDetailPresenter() {
-        return Mockito.mock(NoteDetailPresenter.class);
-    }
-
-    @Provides
-    @ActivityScope
-    NotesPresenter provideNotesPresenter() {
-        return Mockito.mock(NotesPresenter.class);
-    }
-
-    @Provides
-    @ActivityScope
-    RegisterPresenter provideRegisterPresenter() {
-        return Mockito.mock(RegisterPresenter.class);
-    }
-
-    @Provides
-    @ActivityScope
-    SettingsPresenter provideSettingsPresenter() {
-        return Mockito.mock(SettingsPresenter.class);
-    }
-
-    @Provides
-    @ActivityScope
-    NoteEditPresenter provideNoteEditPresenter() {
-        return Mockito.mock(NoteEditPresenter.class);
-    }
-
-    @Provides
-    @ActivityScope
-    ResetPasswordPresenter provideResetPasswordPresenter() {
-        return Mockito.mock(ResetPasswordPresenter.class);
     }
 
 }
