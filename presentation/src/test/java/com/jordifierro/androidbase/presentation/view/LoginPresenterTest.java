6789e20770411565c30ee7354782df8251dd8e51
TonyTandAndroid <tonytangandroid@gmail.com>
2020-01-29 18:53:49 -0800

    Add dummy test to verify Travis CI

--- presentation/src/test/java/com/jordifierro/androidbase/presentation/view/LoginPresenterTest.java
+++ presentation/src/test/java/com/jordifierro/androidbase/presentation/view/LoginPresenterTest.java
@@ -14,6 +14,7 @@ import io.reactivex.Observable;
 import static junit.framework.Assert.assertNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 public class LoginPresenterTest {
 
@@ -80,6 +81,7 @@ public class LoginPresenterTest {
 
         verify(this.mockLoginView).hideLoader();
         verify(this.mockLoginView).viewNotes();
+        verifyNoMoreInteractions(this.mockLoginView);
     }
 
 }
